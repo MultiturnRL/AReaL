f81f5a6eed3da58214aa8c7b6852b422d8c6f462
David Andrews <dha@xoba.com>
2025-11-02 06:17:11 +0000

    added better setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
-bash examples/env/setup-pip-deps.sh
-pip install mcp reasoning-gym anthropic math_verify
+set -eoux pipefail
+
+echo "export HF_HOME=/workspace/hf" >> ~/.bashrc
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv -p 3.12 --seed --clear
+uv run --no-sync bash examples/env/setup-pip-deps.sh
+uv pip install "mcp[cli]" reasoning-gym anthropic math_verify fastmcp
